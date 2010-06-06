da485e10f14cfc9941f860e7e05a31b588d6609a
Shaun Wilde <shaun_wilde@hotmail.com>
2010-06-06 17:27:57 +1000

    update ignore files

--- partcover/PartCover.CorDriver/PartCover.CorDriver.h
+++ partcover/PartCover.CorDriver/PartCover.CorDriver.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Jun 06 07:56:22 2010
+/* at Sun Jun 06 15:39:38 2010
  */
 /* Compiler settings for PartCover.CorDriver.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
