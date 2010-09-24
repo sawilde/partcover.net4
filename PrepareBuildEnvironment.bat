fc474761bdf80e713b5791befcf9f2bdf1253ecc
Tim Murphy <tim@msolutions.com.au>
2010-09-25 07:09:06 +1000

    Created automated process to prepare the build environment.

---
+++ PrepareBuildEnvironment.bat
@@ -0,0 +1,6 @@
+md ..\..\..\libraries
+md ..\..\..\libraries\atlsvr
+md ..\..\..\libraries\atlsvr\atlsvr_9_0_70425
+md ..\..\..\libraries\boost
+libraries\7-Zip\7za x libraries\atlsvr\ATL_Server_Source_and_Headers_9_0_70425_Alpha.zip -o..\..\..\libraries\atlsvr\atlsvr_9_0_70425
+libraries\7-Zip\7za x libraries\boost\boost_1_43_0.zip -o..\..\..\libraries\boost
