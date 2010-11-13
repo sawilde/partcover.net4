f8edcd179675bdb6704dc224ac7bb69991609cad
Shaun Wilde <shaun_wilde at hotmail.com>
2010-11-13 20:44:15 +1100

    set exe to x86; add sample Moq test

--- main/PartCover.StressTest/Program.cs
+++ main/PartCover.StressTest/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using PartCover.StressTest.Classes;
-
+using Moq;
 namespace PartCover.StressTest
 {
     class Program
     {
+        interface IDo
+        {
+            int Something(string data);
+        }
+
         static void Main(string[] args)
         {
             new Line001().Foo();
@@ -13,6 +18,10 @@ namespace PartCover.StressTest
             new GenericClass<int>(1).DoAction<string>("somedata");
             new Subclass().Method("data");
             new SubclassGeneric<int>().Method(1);
+
+            var mock = new Mock<IDo>();
+            mock.Setup(x => x.Something(It.IsAny<string>()))
+                .Returns<string>(s => 1);
 
         }
     }
