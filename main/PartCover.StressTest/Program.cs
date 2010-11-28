e1ef8ffcd0786461e6849d89f5ac2ac89f5c26f3
Shaun Wilde <shaun_wilde at hotmail.com>
2010-11-28 15:46:47 +1100

    fix typo

--- main/PartCover.StressTest/Program.cs
+++ main/PartCover.StressTest/Program.cs
@@ -12,7 +12,7 @@ namespace PartCover.StressTest
 
         static void Main(string[] args)
         {
-            new Line001().Foo();
+            //new Line001().Foo();
             new BigMethod().Foo(new Random(DateTime.Now.Millisecond).Next(255));
             new GenericClass<string>("x").DoAction<float>(1);
             new GenericClass<int>(1).DoAction<string>("somedata");
