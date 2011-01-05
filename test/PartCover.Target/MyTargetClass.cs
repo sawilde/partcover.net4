b024e85e0e716bc59b6e0ada7554ddc721e15b93
Shaun Wilde <shaun_wilde at hotmail.com>
2011-01-05 22:38:18 +1100

    added test class and target to investigate mstest issue

---
+++ test/PartCover.Target/MyTargetClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartCover.Target
+{
+    public class MyTargetClass
+    {
+        public static void MyMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Diagnostics.Trace.WriteLine(string.Format("i={0}", i));
+            }
+        }
+    }
+}
---
+++ test/PartCover.TestTarget.MSTest/BasicUnitTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartCover.Target;
+
+namespace PartCover.TestTarget.MSTest
+{
+    
+
+    [TestClass]
+    public class BasicUnitTestFixture
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            MyTargetClass.MyMethod();
+            Assert.Fail("Arrgghhh!");
+        }
+    }
+}
